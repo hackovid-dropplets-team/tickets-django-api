a564ff0865022fecb0edc86e5545ecec2d12bd2a
CarlesLopezMagem <carleslopezmagem@hotmail.com>
2020-04-12 12:31:49 +0200

    zip code

--- tickets/models.py
+++ tickets/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import RegexValidator
 
 
 class Ticket(models.Model):
@@ -10,6 +11,8 @@ class Ticket(models.Model):
         verbose_name='Publicat', auto_now_add=True)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL,
                               on_delete=models.CASCADE, default=None)
+    zip_code = models.CharField(verbose_name='Codi Postal', max_length=10, validators=[
+                                RegexValidator('^\d{5}(?:[-\s]\d{4})?$')])
     # voluntary = models.ForeignKey(
     #     settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='tickets_as_voluntary', related_query_name='ticket_as_voluntary', default=None)
 
--- tickets/views.py
+++ tickets/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from django.db.models import F, Func
 
 
 class TicketsViewSet(viewsets.ModelViewSet):
@@ -19,12 +20,24 @@ class TicketsViewSet(viewsets.ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-    # def get_permissions(self):
-    #     if self.action == 'create':
-    #         permission_classes = [IsAuthenticated]
-    #     else:
-    #         permission_classes = []
-    #     return [permission() for permission in permission_classes]
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = models.Ticket.objects.all()
+        order_by = ['-published']
+        zip_code = self.request.query_params.get('zip', None)
+        if zip_code is not None:
+            queryset = queryset.annotate(
+                score=(
+                    Func(
+                        (F('zip_code') - zip_code),
+                        function='ABS'
+                    )
+                ))
+            order_by = ['score', *order_by]
+        return queryset.order_by(*order_by)
 
 
 class VolunteeringViewSet(viewsets.ModelViewSet):
